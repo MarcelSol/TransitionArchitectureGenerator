e96c975e88033fcd4da1908472cf67c9c203fd5b
Marcel Sol <marcel@marcelyk.com>
2026-08-09 17:20:45 +0300

    Place nodes by complexity level

--- TransitionArchitectureGenerator/tests/layout/test_placer.py
+++ TransitionArchitectureGenerator/tests/layout/test_placer.py
@@ -2,38 +2,53 @@ from tag.layout.graph import LayoutGraph, LayoutNode
 from tag.layout.placer import LayoutPlacer
 
 
-def test_places_core_nodes():
+def test_places_nodes_by_complexity():
+
     graph = LayoutGraph(
-        nodes={
-            "A": LayoutNode(
-                id="A",
-                name="A",
-                neighbours={"B"},
-                complexity=3,
-            ),
-            "B": LayoutNode(
-                id="B",
-                name="B",
-                neighbours={"A"},
-                complexity=3,
-            ),
-            "C": LayoutNode(
-                id="C",
-                name="C",
-                neighbours={"A"},
-                complexity=2,
-            ),
-        }
+    nodes={
+        "A": LayoutNode(
+            id="A",
+            name="A",
+            neighbours={"B", "C"},
+            complexity=4,
+        ),
+        "B": LayoutNode(
+            id="B",
+            name="B",
+            neighbours={"A", "C"},
+            complexity=4,
+        ),
+        "C": LayoutNode(
+            id="C",
+            name="C",
+            neighbours={"A", "B", "D"},
+            complexity=4,
+        ),
+        "D": LayoutNode(
+            id="D",
+            name="D",
+            neighbours={"C"},
+            complexity=3,
+        ),
+        "E": LayoutNode(
+            id="E",
+            name="E",
+            neighbours={"C"},
+            complexity=2,
+        ),
+    }
     )
 
-    print("Restuld")
-    print( graph)
     placer = LayoutPlacer()
     positions = placer.place(graph)
 
-    assert set(positions) == {"A", "B"}
+    assert set(positions) == {"A", "B", "C", "D", "E"}
+    
     assert positions["A"].layout_layer == 1
     assert positions["B"].layout_layer == 1
+    assert positions["C"].layout_layer == 1
+    assert positions["D"].layout_layer == 1
+    assert positions["E"].layout_layer == 1
 
     print("PASS: core nodes were placed")
 
@@ -46,4 +61,5 @@ def test_places_core_nodes():
         )
 
 
-test_places_core_nodes()
+if __name__ == "__main__":
+    test_places_nodes_by_complexity()
